65a231f2adcf86633b4f72802e0db0b53945fcb6
VV-cephei <dmkueper@gmail.com>
2018-12-29 14:24:44 -0500

    Update quote.js

--- quote.js
+++ quote.js
@@ -3,13 +3,18 @@ var minimist = require('minimist-string');
 
 function quote(command) {
 	var argv = minimist(command);
-	var data = fs.readFileSync('./Full Text - Timestamps - Neutral Names.txt', "utf16le")
+	var data = fs.readFileSync('Full Text - Timestamps - Neutral Names.txt', "utf16le")
+	var data = fs.readFileSync('Full Text - Timestamps - Neutral Names.txt', "utf16le")
 	console.log('in quote command');
 	var quoteName = (argv.name === undefined) ? "" : argv.name;
 	var regexString = "\\n(?=" + quoteName + "\\w*\\s{0,2}\\w*:)";
+	//var regexString = "\\n(?=\\W*\\w*)*?:(\\W*\\w*)*?:\\d*\\s*\\w*\\W*" + quoteName + "\\w*\\s{0,2}\\w*:)";
 	var re = new RegExp(regexString,'gi');
 	var allQuotes = data.split(re);
 	var postId = (argv.id === undefined) ? Math.floor(Math.random() * allQuotes.length) : argv.id;
+	var searchArr = [];
+	var searchArrId = [];
+	var searchCount = 0;
 	regexString = "\\n(?=\\w*\\s{0,2}\\w*:)";
 	re = new RegExp(regexString,'gi');
 
@@ -20,18 +25,33 @@ function quote(command) {
 				quote = allQuotes[postId + i].split(re);
 			}
 			catch (err) {
+				console.log(err);
 				message = "";
 			}
 			message += "(Post# " + (postId + i) + ")" + quote[0] + "\n";
 		}
 		console.log(message);
 		return(message);
+	} else if (argv.search != undefined) {
+		var message = '';
+		for (var i = 0; i < allQuotes.length; i++) {
+			var doesHave = allQuotes[i].search(argv.search);
+			if (doesHave != -1) {
+				searchCount += 1;
+				searchArr.push(allQuotes[i]);
+				searchArrId.push(i);
+			}
+		}
+		postId = (argv.id === undefined) ? Math.floor(Math.random() * searchArr.length) : argv.id - 1;
+		console.log("done");
+		message += "The word " + argv.search + " appeared " + searchCount + " times" + "\n" + "(Post# " + (searchArrId[postId]) + ")" + searchArr[postId];
+		return(message);
 	} else {
 		try {
 			quote = allQuotes[postId].split(re);
 		}
 		catch (err) {
-			//console.log(err);
+			console.log(err);
 			return "";
 		}
 		console.log(quote[0]);
